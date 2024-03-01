aba05674528e464214b57e0ded06ac2da94403e7
KirtimaanSingh <kirtimaansingh01@gmail.com>
2024-03-01 18:33:48 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,9 @@
+book_my_show_link = "https://in.bookmyshow.com/buytickets/dune-part-two-imax-chennai/movie-chen-ET00387000-MT/20240301" #book my show page link which shows datewise showtimes
+
+date = '04' #change to date for search
+
+email_notif_receiver = 'cs9769@srmist.edu.in' #change to email you want the notification mail sent to
+
+mail_message = "Subject: Dune Part Two IMAX Chennai\n\nThe date 04 is available now!" #set your custom mail contents
+
+loop_time = 600 #recheck time in seconds
---
+++ main.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import smtplib
+from credentials import sender_email, sender_password
+from config import book_my_show_link, date, email_notif_receiver, mail_message, loop_time
+
+def send_email(receiver_email):
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, receiver_email, mail_message)
+
+def check_website():
+    url = book_my_show_link
+    
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.get(url)
+    
+    wait = WebDriverWait(driver, 20)
+    date_numeric_div = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "date-numeric")))
+    
+    if date_numeric_div.text.strip() == date:
+        print("Available")
+        driver.quit()
+        return True
+
+    print("Not Available")
+    driver.quit()
+    return False
+
+def main():
+    while True:
+        if check_website():
+            send_email(email_notif_receiver)
+            break
+        time.sleep(loop_time)
+
+if __name__ == "__main__":
+    main()
